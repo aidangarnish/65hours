5356d26db0dcfe7a714ce0f94a2285762d41803d
Aidan Garnish <garnish@outlook.com>
2014-10-24 10:11:30 +0100

    First Commit

---
+++ 65hoursMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_65hoursMVC.MvcApplication" Language="C#" %>
---
+++ 65hoursMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_65hoursMVC.Startup))]
+namespace _65hoursMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
